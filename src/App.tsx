228adf61045fa29ec236d514c7c93a054da35af2
Suryansh <suryansh@techasoft.com>
2024-11-25 14:26:57 +0530

    Delete/Edit functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ interface Todo {
   id: number;
   text: string;
   completed: boolean;
+  isEditing?: boolean;
 }
 
 const App: React.FC = () => {
@@ -26,6 +27,26 @@ const App: React.FC = () => {
     );
   };
 
+  const toggleEdit = (id: number) => {
+    setTodos(
+      todos.map(todo => todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo)
+    );
+  };
+
+  const updateTodo = (id: number, updatedText: string) => {
+    setTodos(
+      todos.map(todo => 
+        todo.id === id ? { ...todo, text: updatedText, isEditing: false} : todo
+      )
+    )
+  }
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+    console.log("Id",id);
+    console.log("Todos",todos);
+  };
+
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
       <h1>To-Do List</h1>
@@ -34,15 +55,34 @@ const App: React.FC = () => {
         onClick={addTodo}>
           Add
       </button>
+      
       <ul>
         {todos.map((todo) => (
-          <li 
-            key={todo.id} 
-            onClick={() => toggleTodo(todo.id)} 
-            style={{
-              textDecoration: todo.completed ? "line-through" : "none", cursor: "pointer",
-            }}
-          >{todo.text}</li>
+          <li key={todo.id}>
+            {todo.isEditing ? (
+              <input 
+              type="text"
+              defaultValue={todo.text}
+              onBlur={(e) => updateTodo(todo.id, e.target.value)}
+              />
+            ) : (
+              <span 
+                style={{
+                  textDecoration: todo.completed ? "line-through" : "none", 
+                  cursor: "pointer",
+                }}
+                onClick={() => toggleEdit(todo.id)}
+              >
+                {todo.text}
+              </span>
+            )}
+            <button onClick={(e) => 
+               { e.stopPropagation(); // To prevent event building
+              deleteTodo(todo.id); }}
+               style={{ marginLeft: "10px" }}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
